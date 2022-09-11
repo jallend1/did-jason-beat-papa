a0f06cb9761f5b23ae658949a341f57a3be6e6cd
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-09-11 10:14:51 -0700

    Adds screenshots of video for each of the game states as placeholder while video is loading

--- src/Components/BackgroundVideo.jsx
+++ src/Components/BackgroundVideo.jsx
@@ -1,21 +1,25 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 
-import VictoryVideo from "../assets/videos/win.mp4";
-import DefeatVideo from "../assets/videos/defeat.mp4";
-import LoadingVideo from "../assets/videos/loading.mp4";
-import NotYetVideo from "../assets/videos/notyet.mp4";
-import DrawVideo from "../assets/videos/draw.mp4";
+import VictoryVideo from '../assets/videos/win.mp4';
+import DefeatVideo from '../assets/videos/defeat.mp4';
+import LoadingVideo from '../assets/videos/loading.mp4';
+import NotYetVideo from '../assets/videos/notyet.mp4';
+import DrawVideo from '../assets/videos/draw.mp4';
 
-import loadingScreenshot from "../assets/images/screenshot-loading.jpg";
+import loadingScreenshot from '../assets/images/screenshot-loading.jpg';
+import notYetScreenshot from '../assets/images/screenshot-not-yet.png';
+import victoryScreenshot from '../assets/images/screenshot-win.jpg';
+import defeatScreenshot from '../assets/images/screenshot-defeat.png';
+import drawScreenshot from '../assets/images/screenshot-draw.png';
 
 const BackgroundVideo = ({ gameResults, isReadyToLoadVideo }) => {
   const [isVideoLoaded, setIsVideoLoaded] = useState(false);
   const backgroundVideos = {
-    win: VictoryVideo,
-    loss: DefeatVideo,
-    pending: NotYetVideo,
-    loading: LoadingVideo,
-    draw: DrawVideo,
+    win: { video: VictoryVideo, screenshot: victoryScreenshot },
+    loss: { video: DefeatVideo, screenshot: defeatScreenshot },
+    pending: { video: NotYetVideo, screenshot: notYetScreenshot },
+    loading: { video: LoadingVideo, screenshot: loadingScreenshot },
+    draw: { video: DrawVideo, screenshot: drawScreenshot }
   };
 
   const onLoadedData = () => {
@@ -27,12 +31,18 @@ const BackgroundVideo = ({ gameResults, isReadyToLoadVideo }) => {
   if (isReadyToLoadVideo) {
     return (
       <div className="background-video" key={gameResults}>
+        <img
+          src={backgroundVideos[gameResults].screenshot}
+          alt="loading screenshot"
+          style={{ opacity: isVideoLoaded ? 0 : 1 }}
+          className="background"
+        />
         <video
           autoPlay
           muted
           loop
-          className={gameResults === "pending" ? "pending-video" : null}
-          src={backgroundVideos[gameResults]}
+          className={gameResults === 'pending' ? 'pending-video' : null}
+          src={backgroundVideos[gameResults].video}
           type="video/mp4"
           ref={videoRef}
         />
